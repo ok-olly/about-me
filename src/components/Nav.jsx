3b7628644e08c2bafb99bceb0427fe4a66ee74b6
ok-olly <jeongoklee0108@gmail.com>
2023-11-28 02:30:52 +0900

    기술 아이콘 추가

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,7 @@ function Nav() {
     "mb-5 text-center text-5xl font-bold uppercase italic tracking-widest hover:not-italic hover:text-lime-50 hover:underline hover:decoration-wavy hover:underline-offset-8";
 
   return (
-    <nav className="animate-bg-gradient absolute left-0 top-0 z-20 h-screen w-screen bg-gradient-to-br from-violet-300 via-amber-200 to-lime-500 bg-[length:400%_400%] font-mono">
+    <nav className="absolute left-0 top-0 z-20 h-screen w-screen animate-bg-gradient bg-gradient-to-br from-violet-300 via-amber-200 to-lime-500 bg-[length:400%_400%] font-mono">
       <ul className="absolute left-1/2 top-1/2 w-fit -translate-x-1/2 -translate-y-1/2">
         <li className={style}>
           <Link to="/" className="block">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,24 @@
+import {
+  SiHtml5,
+  SiCss3,
+  SiSass,
+  SiStyledcomponents,
+  SiTailwindcss,
+  SiJavascript,
+  SiReact,
+  SiRedux,
+  SiGit,
+  // SiMysql,
+  // SiLeaflet,
+  // SiSupabase,
+} from "react-icons/si";
+
 import Heading from "../components/Heading";
 import ArrowButton from "../components/ArrowButton";
 
 function Home() {
   return (
-    <div className="px-3 text-center">
+    <div className="relative flex w-full flex-col gap-5 px-3 text-center">
       <div className="leading-relaxed">
         <p>안녕하세요!</p>
 
@@ -14,7 +29,23 @@ function Home() {
         </div>
       </div>
 
-      <div className="mt-5">
+      <div className="absolute -top-10 left-1/2 grid w-80 -translate-x-1/2 grid-cols-3 justify-items-center gap-y-9 px-3 text-5xl opacity-[.3] sm:-top-20 sm:w-1/2 sm:gap-y-20">
+        <SiHtml5 />
+        <SiCss3 />
+        <SiSass />
+        <SiStyledcomponents />
+        <SiTailwindcss />
+        <SiJavascript />
+        <SiReact />
+        <SiRedux />
+        <SiGit />
+
+        {/* <SiMysql />
+        <SiLeaflet />
+        <SiSupabase /> */}
+      </div>
+
+      <div className="z-10">
         <ArrowButton to="/about">더 알아보기</ArrowButton>
       </div>
     </div>
